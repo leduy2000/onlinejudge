933fc46f6cee555bb4fd24c62855830bbde1fe41
Duy Lê <leduy141000@gmail.com>
2022-09-26 21:24:45 +0700

    zz

--- app/views/problemset/problemset.php
+++ app/views/problemset/problemset.php
@@ -1,4 +1,35 @@
-<div>This is problemset</div>
+<div class="content">
+    <div class="container">
+        <div class="problem-label plT plB">Problems</div>
+        <div class="ui-card url">
+            <div class="flex1">
+                <div>
+                    <div class="problem-title psB">Hello World Contest #1: The Third Three Number Problem</div>
+                    <div> <span id="difficulty" style="color: #d11534">Expert, </span> <span>Max Score: 150</span> </div>
+                </div>
+                <button class="btn-normal ui-btn btn-white msL">Solve Challenge</button>
+            </div>
+        </div>
+        <div class="ui-card url">
+            <div class="flex1">
+                <div>
+                    <div class="problem-title psB">Hello World Contest #1: The Third Three Number Problem</div>
+                    <div> <span id="difficulty" style="color: #d11534">Expert, </span> <span>Max Score: 150</span> </div>
+                </div>
+                <button class="btn-normal ui-btn btn-white msL">Solve Challenge</button>
+            </div>
+        </div>
+        <div class="ui-card url">
+            <div class="flex1">
+                <div>
+                    <div class="problem-title psB">Hello World Contest #1: The Third Three Number Problem</div>
+                    <div> <span id="difficulty" style="color: #d11534">Expert, </span> <span>Max Score: 150</span> </div>
+                </div>
+                <button class="btn-normal ui-btn btn-white msL">Solve Challenge</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script>
     var username = '<?php
